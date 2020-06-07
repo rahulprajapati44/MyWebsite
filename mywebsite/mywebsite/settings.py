f29c3655a0bd895f2a28292b6d943c00fca590a1
rahul <prajapatiravindra44@gmail.com>
2020-06-07 21:03:08 +0530

    ran collectstatic via manage.py

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -123,4 +123,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'staticfiles/images')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"staticfiles")
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
